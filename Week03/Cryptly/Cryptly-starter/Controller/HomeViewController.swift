565afaac80439b5c67f35d2e34e6ca824faa8d71
Shruti Sharma <shrutisharma.la15@gmail.com>
2020-06-12 09:55:50 -0700

    Updates HomeViewController for changes to setupviews and updating data.

--- Week03/Cryptly/Cryptly-starter/Controller/HomeViewController.swift
+++ Week03/Cryptly/Cryptly-starter/Controller/HomeViewController.swift
@@ -34,7 +34,9 @@ import UIKit
 
 class HomeViewController: UIViewController{
 
-  @IBOutlet weak var view1: UIView!
+  //MARK:- IBOutlets
+  
+  @IBOutlet weak var view1: CryptoCurrencyView!
   @IBOutlet weak var view2: UIView!
   @IBOutlet weak var view3: UIView!
   @IBOutlet weak var headingLabel: UILabel!
@@ -42,50 +44,43 @@ class HomeViewController: UIViewController{
   @IBOutlet weak var view2TextLabel: UILabel!
   @IBOutlet weak var view3TextLabel: UILabel!
   @IBOutlet weak var themeSwitch: UISwitch!
-    
+  @IBOutlet weak var mostFallingView: UIView!
+  @IBOutlet weak var mostRisingView: UIView!
+  @IBOutlet weak var mostRisingLabel: UILabel!
+  @IBOutlet weak var mostFallingLabel: UILabel!
+  @IBOutlet weak var mostFallingDataTextLabel: UILabel!
+  @IBOutlet weak var mostRisingDataTextLabel: UILabel!
+
+  //MARK: - Properties
+  let cryptoData = DataGenerator.shared.generateData()
+  
+  //MARK:- View Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    setupViews()
     setupLabels()
+    
+    guard let _ = cryptoData else {
+      return
+    }
+    switchPressed(themeSwitch!)
     setView1Data()
     setView2Data()
     setView3Data()
+    setMostFallingMostRisingData()
   }
-  
+    
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    registerForTheme()
   }
   
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
+    unregisterForTheme()
   }
 
-  func setupViews() {
-      
-    view1.backgroundColor = .systemGray6
-    view1.layer.borderColor = UIColor.lightGray.cgColor
-    view1.layer.borderWidth = 1.0
-    view1.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
-    view1.layer.shadowOffset = CGSize(width: 0, height: 2)
-    view1.layer.shadowRadius = 4
-    view1.layer.shadowOpacity = 0.8
-    
-    view2.backgroundColor = .systemGray6
-    view2.layer.borderColor = UIColor.lightGray.cgColor
-    view2.layer.borderWidth = 1.0
-    view2.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
-    view2.layer.shadowOffset = CGSize(width: 0, height: 2)
-    view2.layer.shadowRadius = 4
-    view2.layer.shadowOpacity = 0.8
-    
-    view3.backgroundColor = .systemGray6
-    view3.layer.borderColor = UIColor.lightGray.cgColor
-    view3.layer.borderWidth = 1.0
-    view3.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
-    view3.layer.shadowOffset = CGSize(width: 0, height: 2)
-    view3.layer.shadowRadius = 4
-    view3.layer.shadowOpacity = 0.8
-  }
+  //MARK:- View Setup
   
   func setupLabels() {
     headingLabel.font = UIFont.systemFont(ofSize: 20, weight: .medium)
@@ -94,14 +89,92 @@ class HomeViewController: UIViewController{
   }
   
   func setView1Data() {
+    guard let cryptoData = cryptoData else {
+      return
+    }
+    view1TextLabel.text = cryptoData.map{ $0.name }.joined(separator: ", ")
   }
   
   func setView2Data() {
+    guard let cryptoData = cryptoData else {
+      return
+    }
+    view2TextLabel.text = cryptoData.filter{ $0.currentValue > $0.previousValue }.map{ $0.name }.joined(separator: ", ")
   }
   
   func setView3Data() {
+    guard let cryptoData = cryptoData else {
+      return
+    }
+    view3TextLabel.text = cryptoData.filter{ $0.currentValue < $0.previousValue }.map{ $0.name }.joined(separator: ", ")
   }
   
-  @IBAction func switchPressed(_ sender: Any) {
+  func setMostFallingMostRisingData() {
+    guard let cryptoData = cryptoData else {
+      return
+    }
+    let mostRising = cryptoData.filter{ $0.trend == .rising }.map{ $0.percentageRise }.max()
+    mostRisingDataTextLabel.text = "\(mostRising!)"
+    
+    let mostFalling = cryptoData.filter{ $0.trend == .falling }.map{ $0.percentageRise }.min()
+    mostFallingDataTextLabel.text = "\(mostFalling!)"
   }
+  
+  //MARK:- IBActions
+
+  @IBAction func switchPressed(_ sender: Any) {
+    if themeSwitch.isOn {
+      ThemeManager.shared.set(theme: DarkTheme())
+    }
+    else {
+      ThemeManager.shared.set(theme: LightTheme())
+    }
+  }
+
+}
+
+//MARK: - Themeable
+
+extension HomeViewController: Themeable {
+  
+  func registerForTheme() {
+    NotificationCenter.default.addObserver(self, selector: #selector(themeChanged),
+                                           name: Notification.Name.init("themeChanged"),
+                                           object: nil)
+  }
+  
+  func unregisterForTheme() {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  @objc func themeChanged() {
+    
+    guard let currentTheme = ThemeManager.shared.currentTheme else {
+      return
+    }
+    view1.backgroundColor = currentTheme.widgetBackgroundColor
+    view2.backgroundColor = currentTheme.widgetBackgroundColor
+    view3.backgroundColor = currentTheme.widgetBackgroundColor
+    mostRisingView.backgroundColor = currentTheme.widgetBackgroundColor
+    mostFallingView.backgroundColor = currentTheme.widgetBackgroundColor
+
+    view1.layer.borderColor = currentTheme.borderColor.cgColor
+    view2.layer.borderColor = currentTheme.borderColor.cgColor
+    view3.layer.borderColor = currentTheme.borderColor.cgColor
+    mostRisingView.layer.borderColor = currentTheme.borderColor.cgColor
+    mostFallingView.layer.borderColor = currentTheme.borderColor.cgColor
+
+    view1TextLabel.textColor = currentTheme.textColor
+    view2TextLabel.textColor = currentTheme.textColor
+    view3TextLabel.textColor = currentTheme.textColor
+    headingLabel.textColor = currentTheme.textColor
+    mostRisingDataTextLabel.textColor = currentTheme.textColor
+    mostFallingDataTextLabel.textColor = currentTheme.textColor
+    mostRisingLabel.textColor = currentTheme.textColor
+    mostFallingLabel.textColor = currentTheme.textColor
+
+    view.backgroundColor = currentTheme.backgroundColor
+  }
+  
+  
 }
