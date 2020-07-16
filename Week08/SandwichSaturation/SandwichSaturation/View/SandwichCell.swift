66f5588ac5501213bb9ca5c2740f52213f0893ed
Shruti Sharma <shrutisharma.la15@gmail.com>
2020-07-16 13:15:18 -0700

    Initial Project Commit.

---
+++ Week08/SandwichSaturation/SandwichSaturation/View/SandwichCell.swift
@@ -0,0 +1,38 @@
+//
+//  SandwichCell.swift
+//  SandwichSaturation
+//
+//  Created by Jeff Rames on 7/3/20.
+//  Copyright © 2020 Jeff Rames. All rights reserved.
+//
+
+import UIKit
+
+class SandwichCell: UITableViewCell {
+  @IBOutlet weak var thumbnail: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var sauceLabel: UILabel!
+  var sandwich: SandwichData?
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  func setupCell() {
+    guard let sandwich = sandwich else {
+      thumbnail.image = nil
+      nameLabel.text = ""
+      sauceLabel.text = ""
+      return
+    }
+    
+    thumbnail.image = UIImage.init(imageLiteralResourceName: sandwich.imageName)
+    nameLabel.text = sandwich.name
+    sauceLabel.text = sandwich.sauceAmount.description
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+}
