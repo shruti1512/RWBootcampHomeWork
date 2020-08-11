688f58919485802a59fb6d9a19037698332f0a7e
Shruti Sharma <shrutisharma.la15@gmail.com>
2020-08-10 17:17:22 -0700

    Implements notification view class and adds label and checkmark image.

--- Week11/Animations/Animations/NotificationView.swift
+++ Week11/Animations/Animations/NotificationView.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class NotificationView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupView()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setupView()
+  }
+  
+  private func setupView() {
+    backgroundColor = .systemGray6
+    layer.cornerRadius = 5
+    addNotificationLabel()
+    addCheckmarkImageView()
+  }
+  
+  private func addNotificationLabel() {
+    addSubview(notificationLabel)
+    notificationLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      notificationLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      notificationLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+    ])
+  }
+  
+  private func addCheckmarkImageView() {
+    addSubview(checkMarkImageView)
+    checkMarkImageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      checkMarkImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      checkMarkImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+    ])
+  }
+  
+  private lazy var notificationLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Animation added successfully."
+    label.textColor = .darkGray
+    label.backgroundColor = .clear
+    return label
+  }()
+  
+  private lazy var checkMarkImageView: UIImageView = {
+    let checkMark = UIImageView()
+    checkMark.image = UIImage(systemName: "checkmark.circle.fill")
+    checkMark.tintColor = .green
+    return checkMark
+  }()
+
 
 }
