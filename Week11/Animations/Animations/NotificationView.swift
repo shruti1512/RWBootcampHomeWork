16203d8e735e3eeb86ffad4c662604b0ca13eae2
Shruti Sharma <shrutisharma.la15@gmail.com>
2020-08-10 17:16:01 -0700

    Adds notification view class.

---
+++ Week11/Animations/Animations/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Animations
+//
+//  Created by Shruti Sharma on 8/10/20.
+//  Copyright © 2020 Shruti Sharma. All rights reserved.
+//
+
+import UIKit
+
+class NotificationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
